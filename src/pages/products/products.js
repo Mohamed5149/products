8bb6ad85867cd293082ff7eaa1462e781b045f14
mohamed abd elmalek <mabdelmalek@seamlabs.com>
2021-06-04 10:57:33 +0200

    first commit

---
+++ src/pages/products/products.js
@@ -0,0 +1,13 @@
+
+const Products = () => {
+    return (
+        <div class="album py-5 bg-light">
+            <div class="container">
+                <div class="row">
+                </div>
+            </div>
+        </div>
+    )
+
+}
+export default Products;
